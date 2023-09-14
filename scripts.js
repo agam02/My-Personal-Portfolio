476dbfd4b431f262b6ab314bfbcfbeb9cf2e9ff8
agam02 <97424357+agam02@users.noreply.github.com>
2023-09-14 14:49:17 +0530

    Adding flies

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    let menu=document.querySelector(".menu-links");
+    let icon=document.querySelector(".hamburger-icon");
+     
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
